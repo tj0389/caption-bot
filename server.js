dcd4c0a21a45cb450d0f5799f3a898ce95cdf47d
Tanmay Jain <jaintanmay389@gmail.com>
2020-12-07 01:41:56 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express=require("express");
 let app=express();
+const port=process.env.PORT || 8000;
 
 app.use(function(req,res,next){
     console.log(`${new Date()} - ${req.method} request for ${req.url}`);
@@ -13,7 +14,7 @@ app.get('/',(req,res)=>{
 	res.sendFile(__dirname+'/index.html');
 });
 
-app.listen(8081,function(){
-    console.log("Serving Serving static on http://localhost:8081")
-});
+app.listen(port,() => {
+    console.log(`Running on server at ${port}`);
+})
 
